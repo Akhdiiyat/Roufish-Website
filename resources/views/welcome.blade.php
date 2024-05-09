d86ffe1d0179f668b23ab0221b2a312558f61a02
Reva Rivandi <rivandi37@gmail.com>
2024-05-09 22:06:05 +0700

    fixed navbar (-) hamburger menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,7 @@
             <!-------Navbar-------->
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
-          <a class="navbar-brand me-auto" href="#">Logo</a>
-          
+          <a class="navbar-brand me-auto" href="#"><img src="assets/image/Roufish.png" alt="logo roufish" width="60px"></a>
           <div
             class="offcanvas offcanvas-end"
             tabindex="-1"
@@ -21,7 +20,7 @@
             aria-labelledby="offcanvasNavbarLabel"
           >
             <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Logo</h5>
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel" ><img src="assets/image/Roufish.png" alt="logo roufish" width="60px"></h5>
               <button
                 type="button"
                 class="btn-close"
@@ -43,24 +42,24 @@
                   <a class="nav-link mx-lg-2" href="#">Lelang</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-lg-2" href="#">Tentang</a>
-                  </li>
+                  <a class="nav-link mx-lg-2" href="#">Tentang</a>
+                </li>
                 <li class="nav-item">
                   <a class="nav-link mx-lg-2" href="#">FAQ</a>
                 </li>
               </ul>
             </div>
           </div class="nav-button">
-          <a href="#" class="login-btn">Masuk</a>
-          <a href="#" class="signup-btn">Daftar</a>
-          <button
-            class="navbar-toggler pe-0"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-            aria-label="Toggle navigation"
-          >
+            <a href="#" class="login-btn">Masuk</a>
+            <a href="#" class="signup-btn">Daftar</a>
+            <button
+              class="navbar-toggler pe-0"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+              aria-label="Toggle navigation"
+            >
             <span class="navbar-toggler-icon"></span>
           </button>
         </div>
